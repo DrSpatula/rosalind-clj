5c2c9d1d3ff8602380e568d3ed241de65999c57a
Seamus M. O'Keefe <okeefe77@gmail.com>
2015-12-10 14:23:15 -0500

    Added function to core for reading FASTA formatted data and created sample data text file

--- src/rosalind_clj/core.clj
+++ src/rosalind_clj/core.clj
@@ -1,4 +1,6 @@
-(ns rosalind-clj.core)
+(ns rosalind-clj.core
+  (:require [clojure.string :as str]
+            [clojure.java.io :as io]))
 
 (def dna-bases #{\A \C \G \T})
 (def rna-bases #{\A \C \G \U})
@@ -12,3 +14,13 @@
   (contains? rna-bases base))
 
 (def invalid-rna-base (complement valid-rna-base))
+
+
+(defn import-fasta [filename]
+  (let [file (io/resource filename)
+        raw-txt (slurp file)
+        raw-entries (rest (str/split raw-txt #">"))
+        cleaned-entries (map #(str/split % #"\n") raw-entries)]
+    (reduce (fn [new-map entry]
+              (assoc new-map (first entry) (reduce str (rest entry))))
+              {} cleaned-entries)))
