87bb7b88de1f1eec333446b9fc8d54e817f5901f
Seamus M. O'Keefe <okeefe77@gmail.com>
2016-01-05 13:25:46 -0500

    Solution to 'Consensus and Profile'

--- src/rosalind_clj/cons.clj
+++ src/rosalind_clj/cons.clj
@@ -1,11 +1,38 @@
 (ns rosalind-clj.cons
-  (require [rosalind-clj.dna :as dna]))
+  (require [rosalind-clj.core :as core]
+           [rosalind-clj.dna :as dna]
+           [clojure.string :as str]))
 
 
 (defn pivot-matrix [strands]
-  (partition (dec (count (first strands))) (apply interleave strands)))
+  (partition (count strands) (apply interleave strands)))
+
+
+(defn counts-by-position [strands]
+  (map dna/count-bases (pivot-matrix strands)))
 
 
 (defn consensus [strands]
-  (let [position-counts (map dna/count-bases (pivot-matrix strands))]
-    (map #(key (apply max-key val %)) position-counts)))
+  (let [position-counts (counts-by-position strands) 
+        cons-list (map #(key (apply max-key val %)) position-counts)]
+    (str/join (map #(second (str %)) cons-list))))
+
+
+(defn profile [strands]
+  (let [counts (counts-by-position strands)
+        base-list (map #(keyword (str %)) core/dna-bases)]
+    (reduce 
+      (fn [profile base]
+        (assoc profile base (map #(get % base 0) counts))) 
+      {} base-list)))
+
+
+(defn consensus-and-profile [fasta-filename]
+  (let [strands (vals (core/import-fasta fasta-filename))
+        con (consensus strands)
+        pro (profile strands)]
+    (println con)
+    (println "A:" (str/join " " (:A pro)))
+    (println "C:" (str/join " " (:C pro)))
+    (println "G:" (str/join " " (:G pro)))
+    (println "T:" (str/join " " (:T pro)))))
